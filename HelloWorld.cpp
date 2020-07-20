7ab896b7c79a02d0e629bb8331383beb26c9e51f
Henry8192 <Henry8192@hotmail.com>
2020-07-20 20:16:23 +0800

    A HelloWorld cpp file.

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+//
+//  HelloWorld.cpp
+//  Luogu
+//
+//  Created by Henry on 7/20/20.
+//  Copyright © 2020 Lowiro. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "HelloWorld!" << endl;
+    return 0;
+}
